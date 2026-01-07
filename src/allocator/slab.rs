e5fdc0651d246d5e68a67fff1aca763eecdb8072
Kaioken <raygsm@live.fr>
2026-01-07 19:28:50 +0100

    Add slab and global allocator struct + corrections

--- src/allocator/slab.rs
+++ src/allocator/slab.rs
@@ -1,11 +1,10 @@
 use bitmask_enum::bitmask;
-use core::{mem::size_of, ptr::null_mut, u32};
+use core::{alloc::GlobalAlloc, mem::size_of, ptr::null_mut, u32};
 use x86_64::VirtAddr;
 use core::mem::offset_of;
+use alloc::alloc::Layout;
+use crate::allocator::Locked;
 
-struct List {
-    head: ListNode,
-}
 struct ListNode {
     next: *mut ListNode,
     prev: *mut ListNode,
@@ -37,7 +36,7 @@ impl PageAllocator {
     }
 
     fn allocate_page(&mut self) -> Option<VirtAddr> {
-        if self.next + 4096u64 > self.heap_end {
+        if self.next.as_u64() + 4096 > self.heap_end.as_u64() {
             None
         } else {
             let page = self.next;
@@ -110,28 +109,22 @@ struct Cache {
     prev: Option<*mut Cache>,
 }
 
-static mut KMEM_CACHE: Cache = Cache {
-    slabs_full: ListNode::new(),
-    slabs_partial: ListNode::new(),
-    slabs_free: ListNode::new(),
-    objsize: size_of::<Cache>(),
-    num: 4096 / size_of::<Cache>(),
-    gfporder: 0,
-    flags: Flags::CfgsOffSlab,
-    spinlock: spin::Mutex::new(()),
-    name: "kmem_cache",
-    next: None,
-    prev: None,
-};
-
 impl Cache {
     pub fn new(name: &'static str, size: usize, flags: Flags) -> Cache {
+        let slab_overhead = size_of::<Slab>();
+        let bufctl_size = size_of::<BufCtl>();
+        let available = 4096 - slab_overhead;
+        let num = available / (size + bufctl_size);
+        let objsize = align_up(size, core::mem::align_of::<usize>());
+
+        assert!(num > 0, "Object too large for slab");
+
         let mut cache = Cache {
             slabs_full: ListNode::new(),
             slabs_partial: ListNode::new(),
             slabs_free: ListNode::new(),
-            objsize: size,
-            num: 4096 / size,
+            objsize: objsize,
+            num: num,
             gfporder: 0,
             flags,
             spinlock: spin::Mutex::new(()),
@@ -150,8 +143,29 @@ impl Cache {
     }
 }
 
+struct SlabAllocator {
+    heap_start: usize,
+    heap_size: usize,
+    heap_end: usize,
+    cache_list: Cache,
+}
+
+impl SlabAllocator {
+    pub fn new() -> Self {
+        let kmem_cache = Cache::new("kmem_cache", size_of::<Cache>(), Flags::CfgsOffSlab);
+        SlabAllocator { heap_start: 0, heap_size: 0, heap_end: 0, cache_list: kmem_cache }
+    }
+
+    pub fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.heap_start = heap_start;
+        self.heap_size = heap_size;
+        self.heap_end = heap_start + heap_size;
+    }
+}
+
 unsafe fn kmem_cache_alloc_one(cache: *mut Cache, flag: Flags, page_allocator: &mut PageAllocator) -> *mut u8 {
     unsafe {
+        let _lock = (*cache).spinlock.lock();
         let slabs_partial = &mut (*cache).slabs_partial;
         let mut entry = slabs_partial.next;
 
@@ -211,14 +225,28 @@ fn init_bufctl(cache: &Cache, slab: *mut Slab) {
     }
 }
 
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
+
 unsafe fn alloc_new_slab(page_allocator: &mut PageAllocator, cache: &mut Cache) {
     let page = page_allocator.allocate_page().expect("Error getting page");
     let page_ptr = page.as_u64() as *mut u8;
 
     let slab = page_ptr as *mut Slab;
 
-    let bufctl_size = cache.num * size_of::<usize>();
-    let obj_start = unsafe { page_ptr.add(size_of::<Slab>() + bufctl_size) };
+    let bufctl_size = cache.num * size_of::<BufCtl>();
+    let obj_start_addr = align_up(
+        page_ptr as usize + size_of::<Slab>() + bufctl_size,
+        core::mem::align_of::<usize>(),
+    );
+
+    assert!(
+        obj_start_addr + cache.num * cache.objsize <= page_ptr as usize + 4096,
+        "Slab layout overflows page"
+    );
+
+    let obj_start = obj_start_addr as *mut u8;
 
     unsafe {
         core::ptr::write(slab, Slab::new(obj_start));
@@ -254,5 +282,17 @@ unsafe fn list_entry(entry: *mut ListNode) -> *mut Slab {
         (entry as *mut u8)
         .sub(offset_of!(Slab, list))
         as *mut Slab
+    }
+}
+
+unsafe impl GlobalAlloc for Locked<SlabAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut allocator = self.lock();
+        
+        todo!() 
+    }  
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        todo!()
     }
 }
